f19ef69dd787743da2550658e5998a416d05805c
Brian Kasprzyk <bkasprzyk@outlook.com>
2021-07-28 22:02:48 -0700

    next JS

---
+++ app1.js
@@ -0,0 +1,19 @@
+console.log('Hello WOrld!')
+
+let userName = promt("What is your name?")
+let userAge = promt("How old are you?")
+
+
+
+if(userName == 'Brian'){
+        document.write('<h2>Welcome ' + userName + '</h2>')}
+        else { document.write('<h2>Not Welcome ' + userName + '</h2>')}
+
+
+// Data Types
+
+// String: letters or numbers 
+// number or integer
+// Boolean True or False
+// Null 
+
---
+++ app2.js
@@ -0,0 +1,10 @@
+let userAge = promt("How old are you?")
+
+let userAnswer = promt("Do you wish to join??")
+if (userAnswer.toLowerCase() == 'yes'){
+document.write('APPLY')}
+
+today = Date()
+
+console.log(today)
+
